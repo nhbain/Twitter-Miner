7fd47d639c330bcc5208a26c6ccb6a659a2e54d0
NHB <bain607@gmail.com>
2017-03-25 11:31:16 -0500

    Timing is correct, but GET requests aren't working.

--- twitter.js
+++ twitter.js
@@ -11,7 +11,7 @@ const fs = require('fs')
 var pollInterval = 60 * 1000
 var pollingWindow = 2 * 60 * 1000
 var streamingWindow = 2 * 60 * 1000
-var waitTime = 15 * 60 * 1001 //added 15 extra milliseconds just to be safe on the limit window
+var waitTime = 2 * 60 * 1001 //added 15 extra milliseconds just to be safe on the limit window
 var location = ''
 
 //Twitter API access info
@@ -32,15 +32,15 @@ const client = new Twitter({
 //  1. Go to http://boundingbox.klokantech.com/
 //  2. Set copy/paste to csv raw
 const stream = client.stream('statuses/filter', {
-	track: '@BeyondWland,#Beyond2017'
-	//track: 'Beyond2017'
-	//locations: '34.1083, -117.2898, 20mi' <- this should be the location for the festival
-	// locations: ''
+	// track: '@BeyondWland,#Beyond2017'
+	track: 'JavaScript'
+	//locations: '-117.4699401855,33.9883491527,-117.0991516113,34.1941975383' <- this is the location for the festival
 })
 
 // Reference to example response
 // https://dev.twitter.com/rest/reference/get/statuses/show/id
 var data = []
+var tempData = []
 stream.on('data', (tweet) => {
 	//https://twitter.com/search?q=%23test&src=typd
 	// Process collected data
@@ -75,29 +75,56 @@ stream.on('error', (error) => {
 // NOTE: Can't stream and poll?
 var pollFunction = null
 const poll = () => {
-
+	console.log('Starting polling . . .' + moment().format('LLLL'))
 	var total = data.length
-	var i, tempData, chunk = 180
+	var i, chunk = 180
 	for(i = 0; i < total; i+=chunk){
+		if(i > total){
+			break
+		}
 		//slice array into sub arrays of size <= 180
-		tempData = data.slice(i, i+chunk)
-		console.log('Taking poll . . .')
-		//pollHelper(i, i+(tempData.length - 1))
-		pollFunction = setInterval(() => {
-			console.log('Taking poll . . .')
-			pollHelper(i, i+(tempData.length - 1))
-		}, waitTime)
+		if(data.length > 180){
+			tempData = data.slice(i, i+chunk)
+			pollFunction = setInterval(() => {
+				console.log('Taking poll . . .' + moment().format('LLLL'))
+				pollHelper(i, i+(tempData.length - 1))
+			}, waitTime)
+		}
+		else{
+			console.log('Taking poll . . .' + moment().format('LLLL'))
+			pollHelper(i, data.length - 1)
+		}
+		// tempData = data.slice(i, i+chunk)
+		// console.log('Taking poll . . .')
+		// pollHelper(i, i+(tempData.length - 1))
+		// pollFunction = setInterval(() => {
+		// 	console.log('Taking poll . . .' + moment().format('LLLL'))
+		// 	pollHelper(i, i+(tempData.length - 1))
+		// }, waitTime)
 	}
+	if (data.length/180 > 1){
+		setTimeout(endPolling, waitTime * (data.length/180))
+	}
+	else{
+		setTimeout(endPolling, waitTime)
+	}
+	
 }
 
 // Recursive call to fix issues with async scope loss
 const pollHelper = (current, total) => {
-	client.get('statuses/lookup', { id: data[current].id }, (error, tweet) => {
+	if(current > data.length){
+		endPolling()
+	}
+	// console.log(current + "," + total + "," + data.length + "," + tempData.length)
+
+	client.get('statuses/lookup', {id: data[current].id} , (error, tweet) => {
 		if (error) {
 			console.log(error)
+			console.log(data[current].id + "," + tweet)
 		} else {
+			console.log(data[current].id + "," + tweet)
 			// NOTE: Find out if this maintains ordering else lookup is required
-			// NOTE: Have to start time tracking these as well
 			if (data[current].rt_overtime[data[current].rt_overtime.length - 1] !== tweet.retweet_count) {
 				data[current].rt_overtime.push(tweet.retweet_count)
 			}
@@ -109,7 +136,12 @@ const pollHelper = (current, total) => {
 			data[current].time.push(moment().milliseconds() - startMoment)
 		}
 
-		if (current < total - 1) pollHelper(current + 1, total)
+		if (current < total - 1){
+			pollHelper(current + 1, total)
+		}
+		else{
+			endPolling() // This is just for testing. Remove once GET is working
+		}
 	})
 }
 
@@ -125,7 +157,7 @@ const output = () => {
 }
 
 const endPolling = () => {
-	console.log('Ending polling . . .')
+	console.log('Ending polling . . .'  + moment().format('LLLL'))
 	clearInterval(pollFunction)
 
 	// Third: Write it all to file
@@ -133,21 +165,27 @@ const endPolling = () => {
 	output()
 }
 
+const startPolling = () => {
+	console.log('Starting polling . . .' + moment().format('LLLL'))
+	setTimeout(poll, waitTime)
+	// setTimeout(endPolling, waitTime * (data.length/180))
+}
+
 const endStreaming = () => {
-	console.log('Ending streaming . . .')
+	console.log('Ending streaming . . .' + moment().format('LLLL'))
 	stream.destroy()
 
 	// Second: Begin polling the end it
-	console.log('Waiting 15 minutes to begin polling . . .')
+	console.log('Waiting ' + (waitTime/60000) + ' minutes to begin polling . . .'+ moment().format('LLLL'))
 	setTimeout(poll, waitTime)
-	console.log('Starting polling . . .')
-	setTimeout(poll, waitTime * 2.2)// this would fire off at the same time as the other call.
-	console.log('Starting second poll . . .')
+	// console.log('Starting polling . . .' + moment().format('LLLL'))
+	// setTimeout(poll, waitTime * 2.2)// this would fire off at the same time as the other call.
+	// console.log('Starting second poll . . .'+ moment().format('LLLL'))
 
-	setTimeout(endPolling, waitTime * (data.length/180))
+	// setTimeout(endPolling, waitTime * (data.length/180))
 }
 
 // First: run stream then end it
 var startMoment = moment().millisecond()
-console.log('Starting streaming . . .')
+console.log('Starting streaming . . .' + moment().format('LLLL'))
 setTimeout(endStreaming, streamingWindow)
