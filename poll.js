922fb2b97bc40fb128ca6b0b8530c8900a12b423
NHB <bain607@gmail.com>
2017-04-05 18:19:23 -0500

    Polling not working correctly... pushing values only into last entry.

--- poll.js
+++ poll.js
@@ -14,37 +14,158 @@ const client = new Twitter({
 
 
 //Variables
-var contents;
-var tweets;
-var maxRetweet;
-var maxFavorite;
+var tweets = []
+var maxRetweet
+var minRetweet
+var maxFavorite
+var minFavorite
+var maxTime = 5*60*1000 //five minutes
+var minTime = 1*60*1000 //one minute
+var pollWindow = 1*60*1000 
+
 
 function init(){
-	contents = fs.readFileSync("output_2017-03-25_12-12-BeyondWland.json")
-	tweets = JSON.parse(contents)
-	minFavorite = 1;
-	minRetweet = 1;
-	maxFavorite = 2;
-	maxRetweet = 2;
+	var contents = fs.readFileSync("output_2017-03-25_12-12-BeyondWland.json")
+	var data = JSON.parse(contents)
+	for(j = 0; j < data.length - 1; j++){
+		tweets.push({
+			id: data[j].id,
+			rt_overtime: [data[j].rt_overtime[0]],
+			fav_overtime: [data[j].fav_overtime[0]],
+			time: [data[j].time[0]],
+			user: data[j].user,
+			user_location: data[j].user_location,
+			profile_pic: data[j].profile_pic,
+			followers: data[j].followers,
+			following: data[j].following,
+			text: data[j].text,
+			fixed_text: data[j].fixed_text,
+			coordinates: data[j].coordinates,
+			event_location: data[j].location
+		})
+	}
 
-	
 
-	for(i = 0; i < tweets.length; i++){
-		console.log("ID tweets[0]: ", tweets[i].id)
+	//initialize variables
+	minFavorite = 0
+	minRetweet = 0
+	maxFavorite = 1
+	maxRetweet = 1
+
+	for(i = 0; i < tweets.length - 1; i++){
 		client.get('statuses/lookup', {id: tweets[i].id} , (error, tweet) => {
 			if(error){
 				console.log(error)
 			}
 			else{
-				if(tweet != null){
+				// console.log(tweet)
+				if(tweet != []){
+					console.log("i: ", i)
 					console.log("Found : ", tweets[i].id)
-					if((tweets[i].rt_overtime[0] == 0) && (tweet.retweet_count == 0)){
-						tweets[i].rt_overtime.push()
+					var temp
+
+					//check to see if max values need to be changed.
+					if(tweet.retweet_count > maxRetweet){maxRetweet = tweet.retweet_count}
+					if(tweet.favorite_count > maxFavorite){maxFavorite = tweet.favorite_count}
+
+					if(tweets[i].rt_overtime[0] == tweet.retweet_count){
+						temp = Math.floor(Math.random() * (maxRetweet - minRetweet) + minRetweet) 
+						tweets[i].rt_overtime.push(temp)
 					}
+					else{
+						tweets[i].rt_overtime.push(tweet.retweet_count)
+					}
+
+					if(tweets[i].fav_overtime[0] == tweet.favorite_count){
+						temp = Math.floor(Math.random() * (maxFavorite - minFavorite) + minFavorite)
+						tweets[i].fav_overtime.push(temp)
+					}
+					else{
+						tweets[i].fav_overtime.push(tweet.favorite_count)
+					}
+
+					temp = Math.floor(Math.random() * (maxTime - minTime) + minTime)
+					tweets[i].time.push(temp)
+				}
+				else{
+					console.log("Unable to find tweet with id: ", tweets[i].id)
+					temp = Math.floor(Math.random() * (maxRetweet - minRetweet) + minRetweet) 
+					tweets[i].rt_overtime.push(temp)
+
+					temp = Math.floor(Math.random() * (maxFavorite - minFavorite) + minFavorite)
+					tweets[i].fav_overtime.push(temp)
+
+					temp = Math.floor(Math.random() * (maxTime - minTime) + minTime)
+					tweets[i].time.push(temp)
 				}
 			}
 		})
-	}	
+	}
+	//dump to new file
+	setTimeout(output, pollWindow)
 }
 
-init();
+const output = () => {
+	var json = JSON.stringify(tweets, null, 4)
+	var filename = 'poll_output_' + moment().format('YYYY-MM-DD_HH-mm') + '-BeyondWland' + '.json'
+	fs.writeFile(filename, json, 'utf8', (error) => {
+		if (error) throw error
+		console.log('File saved as: ' + filename)
+		process.exit()
+	})
+}
+
+const poll = () => {
+	for(i = 0; i < tweets.length; i++){
+		client.get('statuses/lookup', {id: tweets[i].id} , (error, tweet) => {
+			if(error){
+				console.log(error)
+			}
+			else{
+				console.log(tweet)
+				if(tweet != []){
+					console.log("Found : ", tweets[i].id)
+					var temp
+
+					//check to see if max values need to be changed.
+					if(tweet.retweet_count > maxRetweet){maxRetweet = tweet.retweet_count}
+					if(tweet.favorite_count > maxFavorite){maxFavorite = tweet.favorite_count}
+
+					if(tweets[i].rt_overtime[0] == tweet.retweet_count){
+						temp = Math.floor(Math.random() * (maxRetweet - minRetweet) + minRetweet) 
+						tweets[i].rt_overtime.push(temp)
+					}
+					else{
+						tweets[i].rt_overtime.push(tweet.retweet_count)
+					}
+
+					if(tweets[i].fav_overtime[0] == tweet.favorite_count){
+						temp = Math.floor(Math.random() * (maxFavorite - minFavorite) + minFavorite)
+						tweets[i].fav_overtime.push(temp)
+					}
+					else{
+						tweets[i].fav_overtime.push(tweet.favorite_count)
+					}
+
+					temp = Math.floor(Math.random() * (maxTime - minTime) + minTime)
+					tweets[i].time.push(temp)
+				}
+				else{
+					console.log("Unable to find tweet with id: ", tweets[i].id)
+					temp = Math.floor(Math.random() * (maxRetweet - minRetweet) + minRetweet) 
+					tweets[i].rt_overtime.push(temp)
+
+					temp = Math.floor(Math.random() * (maxFavorite - minFavorite) + minFavorite)
+					tweets[i].fav_overtime.push(temp)
+
+					temp = Math.floor(Math.random() * (maxTime - minTime) + minTime)
+					tweets[i].time.push(temp)
+				}
+			}
+		})
+	}
+	//dump to new file
+	setTimeout(output, pollWindow)
+}
+
+init()
